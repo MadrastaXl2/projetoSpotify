66f83a996d328b8d1d732f4bb69f7c091e524901
MadrastaXl2 <147931416+MadrastaXl2@users.noreply.github.com>
2025-02-16 18:49:02 -0300

    ajuste no processo .env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -5,8 +5,9 @@
 import axios from "axios";
 import 'dotenv/config'
 
-const {NODE_ENV} = process.env
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+//const {NODE_ENV} = process.env
+//const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+const URL = "https://projetospotify-02mz.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
